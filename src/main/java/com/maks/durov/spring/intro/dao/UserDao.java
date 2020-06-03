a04cc78f0720439d36b7ef31d89063f55c6b843d
MaksimDurov <maksim.durov45@gmail.com>
2020-06-03 12:17:57 +0300

    Hw 30.1 (#1)
    * added Main.java
    
    * added User class UserService interface and UserDao Interface
    
    * added implementatio for user service and user dao interfaces
    
    * added AppConfig.java , added some test in Main.java everything works

---
+++ src/main/java/com/maks/durov/spring/intro/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.maks.durov.spring.intro.dao;
+
+import com.maks.durov.spring.intro.model.User;
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+
+    List<User> getAllUsers();
+}
